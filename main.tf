91d148c5899d1f8fb17c2a9513db3c9a51982ec5
krishna <brkkrishna9@gmail.com>
2023-02-25 21:13:54 +0530

    new changes

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_docdb_subnet_group" "default" {
+resource "aws_db_subnet_group" "default" {
   name       = "${var.env}-docdb-subnet_group"
   subnet_ids = var.subnet_ids
 
@@ -8,15 +8,15 @@ resource "aws_docdb_subnet_group" "default" {
   )
 }
 
-resource "aws_security_group" "docdb" {
-  name        = "${var.env}-docdb-security_group"
-  description = "${var.env}-docdb-security_group"
+resource "aws_security_group" "rds" {
+  name        = "${var.env}-rds-security_group"
+  description = "${var.env}-rds-security_group"
   vpc_id      = var.vpc_id
 
   ingress {
-    description      = "MongoDB"
-    from_port        = 27017
-    to_port          = 27017
+    description      = "RDS"
+    from_port        = 3306
+    to_port          = 3306
     protocol         = "tcp"
     cidr_blocks      = var.allow_cidr
   }
@@ -30,37 +30,23 @@ resource "aws_security_group" "docdb" {
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-docdb-surity_group" }
+    { Name = "${var.env}-rds-surity_group" }
   )
 }
 
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "${var.env}-docdb-cluster"
-  engine                  = "docdb"
+resource "aws_rds_cluster" "rds" {
+  cluster_identifier      = "${var.env}-rds"
+  engine = var.engine
   engine_version = var.engine_version
+  db_cluster_instance_class = var.instance_class
+  storage_type = "io1"
+  allocated_storage = 20
+  iops = 1000
   master_username         = data.aws_ssm_parameter.DB_ADMIN_USER.value
   master_password         = data.aws_ssm_parameter.DB_ADMIN_PASS.value
-  skip_final_snapshot     = true
-  db_subnet_group_name = aws_docdb_subnet_group.default.name
-  vpc_security_group_ids = [aws_security_group.docdb.id]
+  db_subnet_group_name = aws_db_subnet_group.default.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
   storage_encrypted = true
-  kms_key_id = data.aws_kms_key.key.arn
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-docdb-cluster" }
-  )
-}
-
-resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = var.number_of_instances
-  identifier         = "${var.env}-docdb-cluster-instance-${count.index}"
-  cluster_identifier = aws_docdb_cluster.docdb.id
-  instance_class     = var.instance_class
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-docdb-cluster-instance-${count.index}" }
-  )
+  kms_key_id = data.aws_kms_key.key.id
 }
 
--- vars.tf
+++ vars.tf
@@ -2,6 +2,8 @@ variable "env" {}
 variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "allow_cidr" {}
+variable "engine" {}
 variable "engine_version" {}
 variable "number_of_instances" {}
 variable "instance_class" {}
+
