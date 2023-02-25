366f64857532146a604a6444d8d9f2a54f6524a4
krishna <brkkrishna9@gmail.com>
2023-02-26 00:01:42 +0530

    new changes

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_db_subnet_group" "default" {
-  name       = "${var.env}-docdb-subnet_group"
+  name       = "${var.env}-rds-subnet_group"
   subnet_ids = var.subnet_ids
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-docdb-subnet_group" }
+    { Name = "${var.env}-rds-subnet_group" }
   )
 }
 
@@ -38,15 +38,16 @@ resource "aws_rds_cluster" "rds" {
   cluster_identifier      = "${var.env}-rds"
   engine = var.engine
   engine_version = var.engine_version
-  db_cluster_instance_class = var.instance_class
-  storage_type = "io1"
-  allocated_storage = 20
-  iops = 1000
   master_username         = data.aws_ssm_parameter.DB_ADMIN_USER.value
   master_password         = data.aws_ssm_parameter.DB_ADMIN_PASS.value
   db_subnet_group_name = aws_db_subnet_group.default.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   storage_encrypted = true
   kms_key_id = data.aws_kms_key.key.id
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-rds" }
+  )
 }
 
